91ddf3c6ca0fe3ebd8aac4a4a7ca6cf52765ede6
xufengming <xufengming@do1.com.cn>
2021-01-18 19:02:32 +0800

    卡片设计

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,39 +1,65 @@
 <template>
 	<view class="content">
-  <view style="width: 100%;">
-    <tabs :list="tabList"
-      :is-scroll="false"
-      :current="current"
-      height="160"
-      bar-width="100"
-      bar-height="100"
-      bg-color="#5677FC"
-      active-color="#fff"
-      inactive-color="#fff"
-      @change="change"></tabs>
-  </view>
-  <u-input v-model="workType" 
-    type="select"
-    @click="showWorkList = true"/>
-  <u-select 
-    v-model="showWorkList" 
-    mode="single-column" 
-    :list="workList" 
-    @confirm="selectWork"></u-select>
-  <u-input v-model="doneType" 
-    type="select" 
-    @click="showDoneList = true"/>
-  <u-select v-model="showDoneList" 
-    mode="single-column" 
-    :list="doneList" 
-    @confirm="selectDone"></u-select>
-  <view>
-    <text class="title">主页</text>
-  </view>
-  <u-tabbar :list="tabbarList" 
-    :mid-button="true"
-    active-color="#5677FC"
-    inactive-color="#333333"></u-tabbar>
+    <view class="content_header">
+      <view style="width: 100%;">
+        <tabs :list="tabList"
+          :is-scroll="false"
+          :current="current"
+          height="160"
+          bar-width="100"
+          bar-height="100"
+          bg-color="#5677FC"
+          active-color="#fff"
+          inactive-color="#fff"
+          @change="change"></tabs>
+      </view>
+      <view class="select_box">
+        <u-input v-model="workType"
+          class="select_box--input"
+          type="select"
+          border
+          height="50"
+          :custom-style="selectStyle"
+          :select-open="showWorkList"
+          @click="showWorkList = true"/>
+        <u-select 
+          v-model="showWorkList"
+          mode="single-column"
+          :list="workList"
+          :default-value="[0]"
+          @confirm="selectWork"></u-select>
+        <u-input v-model="doneType"
+          class="select_box--input" 
+          type="select"
+          border
+          height="50"
+          :custom-style="selectStyle"
+          :select-open="showDoneList"
+          @click="showDoneList = true"/>
+        <u-select v-model="showDoneList"
+          mode="single-column"
+          :list="doneList"
+          :default-value="[0]"
+          @confirm="selectDone"></u-select>
+      </view>
+    </view>
+    <view class="content_body">
+      <u-card :title="title" :sub-title="subTitle" :thumb="thumb">
+        <view class="" slot="body">
+          <view class="u-body-item u-flex">
+            <view class="u-body-item-title">瓶身描绘的牡丹一如你初妆，冉冉檀香透过窗心事我了然，宣纸上走笔至此搁一半</view>
+            <image src="https://img11.360buyimg.com/n7/jfs/t1/94448/29/2734/524808/5dd4cc16E990dfb6b/59c256f85a8c3757.jpg" mode="aspectFill"></image>
+          </view>
+        </view>
+        <view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
+      </u-card>
+    </view>
+    <view class="content_footer">
+      <u-tabbar :list="tabbarList" 
+      :mid-button="true"
+      active-color="#5677FC"
+      inactive-color="#333333"></u-tabbar>
+    </view>  
 	</view>
 </template>
 
@@ -71,37 +97,44 @@
 					},
 					{
 						value: '1',
-						label: '进行中'
-          },
-          {
-						value: '2',
-						label: '已截至'
-					}
-        ],
-        doneList: [
-          {
-						value: '3',
-						label: '未完成'
-					},
-					{
-						value: '4',
-						label: '已完成'
+						label: '班级作业'
           },
           {
 						value: '2',
 						label: '个人作业'
+					}
+        ],
+        doneList: [
+          {
+						value: '0',
+						label: '全部'
+					},
+					{
+						value: '1',
+						label: '进行中'
+          },
+          {
+						value: '2',
+						label: '未完成'
           },
           {
 						value: '3',
-						label: '班级作业'
+						label: '已完成'
           },
           {
 						value: '4',
-						label: '班级作业'
+						label: '已截止'
           }
         ],
-        workType: '',
-        doneType: ''
+        workType: '全部',
+        doneType: '全部',
+        selectStyle: {
+          // background: '#fff'
+          color: '#515151'
+        },
+        title: '素胚勾勒出青花，笔锋浓转淡',
+        subTitle: '2020-05-15',
+        thumb: 'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg'
 			}
 		},
 		onLoad() {
@@ -121,6 +154,39 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+.select_box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #5677FC;
+  &--input {
+    width: 600rpx;
+    margin-bottom: 20rpx;
+    background: #ffffff;
+    
+    &/deep/ .u-input__right-icon {
+      transform: scale(0.7);
+    }
+  }
+}
 
+.u-card-wrap { 
+		background-color: $u-bg-color;
+		padding: 1px;
+	}
+	
+	.u-body-item {
+		font-size: 32rpx;
+		color: #333;
+		padding: 20rpx 10rpx;
+	}
+		
+	.u-body-item image {
+		width: 120rpx;
+		flex: 0 0 120rpx;
+		height: 120rpx;
+		border-radius: 8rpx;
+		margin-left: 12rpx;
+	}
 </style>
