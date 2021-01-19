8a5b061b605545146e384272981483cdc7be1fc7
xufengming <xufengming@do1.com.cn>
2021-01-19 18:49:29 +0800

    发布作业功能

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -44,22 +44,28 @@
       </view>
     </view>
     <view class="content_body">
-      <u-card :title="title" :sub-title="subTitle" :thumb="thumb">
+      <u-card padding="10">
+        <view class="" slot="head">
+          {{title}}
+          <a>---</a>
+        </view>
         <view class="" slot="body">
           <view class="u-body-item u-flex">
             <view class="u-body-item-title">瓶身描绘的牡丹一如你初妆，冉冉檀香透过窗心事我了然，宣纸上走笔至此搁一半</view>
             <image src="https://img11.360buyimg.com/n7/jfs/t1/94448/29/2734/524808/5dd4cc16E990dfb6b/59c256f85a8c3757.jpg" mode="aspectFill"></image>
           </view>
         </view>
-        <view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
+        <view class="" slot="foot">{{subTitle}}</view>
       </u-card>
     </view>
     <view class="content_footer">
       <u-tabbar :list="tabbarList" 
       :mid-button="true"
       active-color="#5677FC"
-      inactive-color="#333333"></u-tabbar>
-    </view>  
+      inactive-color="#333333"
+      :before-switch="switchAdd"></u-tabbar>
+    </view>
+    <u-picker v-model="showAddwork" mode="selector"></u-picker>
 	</view>
 </template>
 
@@ -134,7 +140,8 @@
         },
         title: '素胚勾勒出青花，笔锋浓转淡',
         subTitle: '2020-05-15',
-        thumb: 'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg'
+        thumb: 'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg',
+        showAddwork: false
 			}
 		},
 		onLoad() {
@@ -149,6 +156,11 @@
       },
       selectDone(e) {
         this.doneType = e[0].label
+      },
+      switchAdd(index) {
+        if (index === 1) {
+          this.showAddwork = true
+        }
       }
 		}
 	}
